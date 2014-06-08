cd6721a38b982a460b5dde534282e8da91e62f63
prasadleelasahu <prasad@dexati.com>
2014-06-08 22:26:48 +0530

    Init country animal quiz

---
+++ National Trees Quiz /src/com/villatech/nationalanimalquiz/CountryFlagModel.java
@@ -0,0 +1,52 @@
+package com.villatech.nationalanimalquiz;
+
+ 
+public class CountryFlagModel {
+	private String mCountryCode;
+	private String mCountryName;
+	private String mGeoLocation;
+	private String mFlagFile;
+
+	public CountryFlagModel(String cc, String cn, String geo, String flagfile) {
+		this.setmCountryCode(cc);
+		this.setmCountryName(cn);
+		this.setmGeoLocation(geo);
+		this.setmFlagFile(flagfile);
+	}
+
+	public CountryFlagModel() {
+	}
+
+	public String getmGeoLocation() {
+		return mGeoLocation;
+	}
+
+	public void setmGeoLocation(String mGeoLocation) {
+		this.mGeoLocation = mGeoLocation;
+	}
+
+	public String getmFlagFile() {
+		return mFlagFile;
+	}
+
+	public void setmFlagFile(String mFlagFile) {
+		this.mFlagFile = mFlagFile;
+	}
+
+	public String getmCountryName() {
+		return mCountryName;
+	}
+
+	public void setmCountryName(String mCountryName) {
+		this.mCountryName = mCountryName;
+	}
+
+	public String getmCountryCode() {
+		return mCountryCode;
+	}
+
+	public void setmCountryCode(String mCountryCode) {
+		this.mCountryCode = mCountryCode;
+	}
+
+}
